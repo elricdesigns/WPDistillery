b0b8390dcfe07861c05afc67a07e3421f583ebda
Elric <spiritofelric@gmail.com>
2017-07-23 00:39:30 -0400

    WPDistillery base setup

--- wpdistillery/provision.sh
+++ wpdistillery/provision.sh
@@ -10,7 +10,7 @@
 # update WP-CLI
 # since Scotch Box 3.0 (php7), we have to reinstall WP-CLI
 echo "== Update WP CLI (re-install) =="
-cd ../../../
+cd /
 sudo rm usr/local/bin/wp
 sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 sudo chmod +x wp-cli.phar
@@ -19,5 +19,5 @@ echo "WP-CLI Update successful"
 
 # run WPDistillery
 echo "== Run WPDistillery =="
-cd ../../var/www/wpdistillery
+cd /var/www/WPDistillery/wpdistillery
 sudo -u vagrant bash wpdistillery.sh
